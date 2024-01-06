6d366906ec074a23dd77b2e0e2faa694d6d66b9e
Vinícius Gonçalves <viniciusdejgoncalves@gmail.com>
2024-01-06 16:46:49 -0300

    added movement with animations

---
+++ Assets/_Project/Scripts/Movement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] List<Animator> animators;
+
+    Vector2 input;
+    Vector2 lookDir;
+    Rigidbody2D rb;
+    bool isMoving;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+        HandleAnimation();
+    }
+
+    private void HandleInput()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void HandleMovement()
+    {
+        rb.velocity = input.normalized * speed * Time.fixedDeltaTime;
+        isMoving = rb.velocity.magnitude > 0;
+    }
+
+    private void HandleAnimation()
+    {
+        if (isMoving)
+            lookDir = input;
+
+        foreach (Animator animator in animators)
+        {
+            if (!animator.isActiveAndEnabled)
+                continue;
+
+            animator.SetFloat("MoveX", lookDir.x);
+            animator.SetFloat("MoveY", lookDir.y);
+
+            animator.SetBool("IsMoving", isMoving);
+        }
+    }
+}
